efb46792a40cfa8f298d22dae2da23091b96101e
María Sánchez Carvajal <maria.carvajal@galpsolar.com>
2024-10-03 10:40:03 +0200

    feat: add check for a column as enum

--- 5. 3. Check con multiples posibilidades strings.sql
+++ 5. 3. Check con multiples posibilidades strings.sql
@@ -2,3 +2,14 @@ SELECT
     *
 FROM
     country;
+
+SELECT DISTINCT continent FROM country;
+-- copiamos los continentes existentes para usarlos en la siguiente query
+
+ALTER TABLE country ADD CHECK(continent IN('Asia',
+'South America',
+'North America',
+'Oceania',
+'Antarctica',
+'Africa',
+'Europe'));
